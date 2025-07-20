f738884a4e3ca24898fa46f1705ce5f5704a067b
Jesse <jessejamesjj007@gmail.com>
2025-07-20 14:23:56 +0300

    Add components

---
+++ src/components/Heading.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+      <h1>SMART GOAL PLANNER</h1>
+      <p>The Smart Goal Planner is a tool that allows users to manage multiple saving goals, allocate deposits across them, and track progress toward each goal.</p>
+      <br />
+      <p>Choose what you would like to do</p>
+      <button><a href="/content/goals.jsx">Goals Form</a></button>
+      <button><a href="/content/progressTrack.jsx">Progress Track</a></button>
+
+    </div>
+  )
+}
+
+export default Heading
